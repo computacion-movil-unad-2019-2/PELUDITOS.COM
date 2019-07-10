20b56600697ddec26558362f0401520b5b6cdb0f
AndresCriollo <andrescriollo.ac7@gmail.com>
2019-07-09 22:54:25 -0500

    Peluditos4

---
+++ Peluditos4/app/src/main/java/com/entidades/CEventos.java
@@ -0,0 +1,50 @@
+package com.entidades;
+
+public class CEventos {
+
+    public String codigo;
+    public String nombre;
+    public String lugar;
+    public String hora;
+    public String fecha;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+}
