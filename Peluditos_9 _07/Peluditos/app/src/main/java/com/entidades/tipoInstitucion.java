e502c82fcdb27dab2f3be0abd96d0e1f421a1292
Alexavane1 <avc1031@hotmail.com>
2019-07-10 12:17:11 -0500

    Funcionalidades AVC  version definitiva
    Copia  de las funcionalidades  Instituciones, Mascota con foto, Adoptante y Adopta  y Agenda

---
+++ Peluditos_9 _07/Peluditos/app/src/main/java/com/entidades/tipoInstitucion.java
@@ -0,0 +1,32 @@
+package com.entidades;
+
+public class tipoInstitucion {
+
+    public String id;
+    public String codigo;
+    public String descripcion;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
